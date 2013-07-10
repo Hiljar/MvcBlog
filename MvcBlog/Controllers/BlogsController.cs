3661241fbc626f20be2d07d032f9841b5d4278f9
Hiljar <hiljar.undrits@gmail.com>
2013-07-10 22:16:10 +0300

    Lae

---
+++ MvcBlog/Controllers/BlogsController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcBlog.Models;
+
+namespace MvcBlog.Controllers
+{
+   
+    public class BlogsController : Controller
+    {
+        private MovieDBContext db = new MovieDBContext();
+        
+    
+        //
+        // GET: /Blogs/
+
+        public ViewResult Index()
+        {
+            return View(db.Blogs.ToList());
+        }
+
+         [Authorize(Roles = "Administrator")]
+    
+    
+        //
+        // GET: /Blogs/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Blog blog = db.Blogs.Find(id);
+            return View(blog);
+        }
+
+
+
+    [Authorize(Roles = "Administrator")]
+    
+        //
+        // GET: /Blogs/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+
+      [Authorize(Roles = "Administrator")]
+    
+        //
+        // POST: /Blogs/Create
+
+        [HttpPost]
+        public ActionResult Create(Blog blog)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(blog);
+        }
+
+          [Authorize(Roles = "Administrator")]
+    
+        //
+        // GET: /Blogs/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Blog blog = db.Blogs.Find(id);
+            return View(blog);
+        }
+
+          [Authorize(Roles = "Administrator")]
+    
+        //
+        // POST: /Blogs/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Blog blog)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(blog).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(blog);
+        }
+
+          [Authorize(Roles = "Administrator")]
+    
+        //
+        // GET: /Blogs/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Blog blog = db.Blogs.Find(id);
+            return View(blog);
+        }
+
+          [Authorize(Roles = "Administrator")]
+    
+        //
+        // POST: /Blogs/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Blog blog = db.Blogs.Find(id);
+            db.Blogs.Remove(blog);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    
+        
+      
+          
+      
+    }
+
+}
---
+++ MvcBlog/Models/Blog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MvcBlog.Models
+{
+    public class Blog
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Post { get; set; }
+       
+    }
+
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+    }
+}
